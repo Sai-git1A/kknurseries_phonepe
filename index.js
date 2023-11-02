a4c9b9cfca92a28c5b82fc6d887dcffc940db7fd
Sai Dharshan <saidharshan018@gmail.com>
2023-11-02 10:13:07 +0530

    Some Major Changes

--- index.js
+++ index.js
@@ -5,66 +5,81 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 const sha256 = require('sha256');
-const uniqid = require('uniqid');
 
 const app = express();
+let user_data = {
+  'name': '',
+  'email': '',
+  'amount': 0,
+  'order_id':'',
+  'address': '',
+  'phone': ''
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
 mongoose.set('strictQuery', false);
-// mongoose.connect(process.env.MONGOURL);
-// mongoose.connection.on('connected', () => {
-//   console.log('database connected');
-// });
+mongoose.connect(process.env.MONGOURL);
+mongoose.connection.on('connected', () => {
+  console.log('database connected');
+});
 
 app.get('/', (req, res) => {
-    res.send('Server running at port 8000...')
+    res.send('Server running Successfully!');
 });
 
 app.post('/place-order', (req, res) => {
-    const merch_tx_id = uniqid();
 
-    const normalPayLoad = {
-      "merchantId": process.env.ID,
-      "merchantTransactionId": merch_tx_id,
-      "merchantUserId": "KKNURSERIES",
-      "amount": req.body.price,
-      "redirectUrl": "https://kknurseries-phonepe.vercel.app/callback/",
-      "redirectMode": "POST",
-      "callbackUrl": "https://kknurseries-phonepe.vercel.app/callback/",
-      "mobileNumber": req.body.phone,
-      "paymentInstrument": {
-        "type": "PAY_PAGE"
-      }
+  user_data = {
+    'name': req.body.name,
+    'email': req.body.email,
+    'amount': req.body.price,
+    'order_id': req.body.orderId,
+    'address': req.body.address,
+    'phone': req.body.phone
+  }
+
+  const normalPayLoad = {
+    "merchantId": process.env.ID,
+    "merchantTransactionId": req.body.orderId,
+    "merchantUserId": "KKNURSERIES",
+    "amount": req.body.price,
+    "redirectUrl": "https://kknurseries-phonepe.vercel.app/callback/",
+    "redirectMode": "POST",
+    "callbackUrl": "https://kknurseries-phonepe.vercel.app/callback/",
+    "mobileNumber": req.body.phone,
+    "paymentInstrument": {
+      "type": "PAY_PAGE"
     }
+  }
 
-    const bufferObj = Buffer.from(JSON.stringify(normalPayLoad), "utf8");
-    const base64String = bufferObj.toString("base64");
-    const string = base64String + '/pg/v1/pay' + process.env.KEY;
-    const sha256_val = sha256(string);
-    const checksum = sha256_val + '###' + 2;
+  const bufferObj = Buffer.from(JSON.stringify(normalPayLoad), "utf8");
+  const base64String = bufferObj.toString("base64");
+  const string = base64String + '/pg/v1/pay' + process.env.KEY;
+  const sha256_val = sha256(string);
+  const checksum = sha256_val + '###' + 2;
 
-    fetch('https://api.phonepe.com/apis/hermes/pg/v1/pay', {
-      method: 'POST',
-      mode: 'no-cors',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-VERIFY': checksum,
-        'accept': 'application/json',
-      },
-      body: JSON.stringify({
-        request: base64String
-      })
+  fetch('https://api.phonepe.com/apis/hermes/pg/v1/pay', {
+    method: 'POST',
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-VERIFY': checksum,
+      'accept': 'application/json',
+    },
+    body: JSON.stringify({
+      request: base64String
     })
-    .then(response => response.json())
-    .then(data => res.send({result: data}))
+  })
+  .then(response => response.json())
+  .then(data => res.send({result: data}))
 });
 
 app.post('/callback', (req, res) => {
   if (req.body.code == 'PAYMENT_SUCCESS' && req.body.merchantId && req.body.transactionId && req.body.providerReferenceId) {
-    if (req.body.transactionId) {
+    if (req.body.transactionId == user_data.order_id && req.body.merchantId == process.env.ID && req.body.amount == user_data.price) {
       const surl = `https://api.phonepe.com/apis/hermes/pg/v1/status/${process.env.ID}/` + req.body.transactionId;
       
       const string = `/pg/v1/status/${process.env.ID}/` + req.body.transactionId + process.env.KEY;
@@ -82,12 +97,13 @@ app.post('/callback', (req, res) => {
         'accept': 'application/json',
       }
     })
-    .then(response => response.json())
-    .then(data => res.send({result: data}))
+    .then(response => res.send(JSON.stringify(response.data)))
     }
+  } else {
+    res.send('Payment Failed!');
   }
 });
 
 app.listen(process.env.PORT || 8000, (req, res) => {
-    console.log('server listening on port 8000...');
+    console.log('server running successfully');
 });
